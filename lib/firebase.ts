f6fc78420e63a453162a2b26813c5aa9140f72a2
Ankur <am8468616@gmail.com>
2025-10-02 23:18:53 +0530

    typos fix

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,19 +1,13 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 
 const firebaseConfig = {
-//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-   apiKey: "AIzaSyBvhpLpdQZ5jh9HcmXl_MS60PGmP715Res",
-  authDomain: "healthbridge-5dee8.firebaseapp.com",
-  projectId: "healthbridge-5dee8",
-  storageBucket: "healthbridge-5dee8.firebasestorage.app",
-  messagingSenderId: "191206764523",
-  appId: "1:191206764523:web:a4933d6c280c35424133ae",
-  measurementId: "G-L0YHG74H0D"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
